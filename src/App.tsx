6c071e19b20e8e8b0b0298a983019ebd56ee1287
vih0 <araujovitoris1.4@gmail.com>
2023-01-26 17:54:51 -0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+export function App() {
+  return (
+    <div className="App">
+      <ThemeProvider theme={defaultTheme}>
+        <Button />
+        <Button variant="secondary" />
+        <Button variant="primary" />
+        <Button variant="success" />
+        <Button variant="danger" />
+        <GlobalStyle />
+      </ThemeProvider>
+    </div>
+  )
+}
