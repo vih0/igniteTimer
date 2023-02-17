80e9fe177be23a50e1e3843dd0ad6816be445dcc
vih0 <araujovitoris1.4@gmail.com>
2023-02-17 13:39:03 -0300

    create History page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,47 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
-  return <h1>oi eu sou historico</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico:</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>tarefa</td>
+              <td>22 minutos</td>
+              <td>2meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>22 minutos</td>
+              <td>2meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>22 minutos</td>
+              <td>2meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>22 minutos</td>
+              <td>2meses</td>
+              <td>Concluido</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
