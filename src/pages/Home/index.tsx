589e35d13620ff663c5de8eb02d693465186b85a
vih0 <araujovitoris1.4@gmail.com>
2023-03-07 15:19:18 -0300

    fix:components container

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,24 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
 import * as S from './style'
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+  const task = watch('task')
   return (
     <S.HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <S.FormContainer>
           <label htmlFor="task">Vou Trabalhar em: </label>
           <S.TaskInput
             id="task"
             list="task-sugestion"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
           <datalist id="task-sugestion">
             <option value="teste1" />
@@ -23,6 +31,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
         </S.FormContainer>
         <S.CountdownContainer>
@@ -32,7 +41,7 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </S.CountdownContainer>
-        <S.StartButton type="submit" disabled>
+        <S.StartButton type="submit" disabled={!task}>
           <Play />
           Começar
         </S.StartButton>
